5dc1aa44b5b3c3eb1c39ffb33b9f0a5ef0d73e84
Jesse Asamoa <jesseasamoa@gmail.com>
2022-09-05 11:22:28 +0000

    initial commit-framework

---
+++ pages/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, TemplateView
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+
+
+class Login(TemplateView):
+    template_name = 'login.html'
+
+    @method_decorator(csrf_protect)
+    def post(self, request):
+        if request.method == 'POST':
+            uname = request.POST.get('uname')
+            password = request.POST.get('password')
+            user = authenticate(request, username=uname, password=password)
+            if user is None:
+                messages.error(request, 'Enter the right credentials!')
+                return render(request, 'login.html')
+            else:
+                login(request, user)
+                # messages.success(request, 'You are successfully loggedin!')
+                return redirect('dashboard')
+
+        return render(request, 'dashboard.html')
+
+
+@csrf_protect
+def logout_page(request):
+    logout(request)
+    messages.success(request, 'You are logged out. Login!')
+    return redirect('login')
+
+
+class Dashboard(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard.html'
+
+
+
+
+
