eaec4f7dfa279bcbf65381a798611ed74752fc55
Jesse Asamoa <jesseasamoa@gmail.com>
2022-09-23 15:25:05 +0000

    updated requirements.txt file

--- pages/test_call.py
+++ pages/test_call.py
@@ -1,12 +1,15 @@
 import call_api
 import json
+import csv
 
-# print(call_api.api_call('reports/types', {}))
-api_data = call_api.api_call("reports/types", {})
+api_data = call_api.api_call('reports/list', {})
 jsonResponse = json.loads(api_data.decode('utf-8'))
-print(jsonResponse)
-# print(type(api_data))
-#
-# with open(api_data) as json_file:
-#     data = json.load(json_file)
-#     print(data)
+file_data = (jsonResponse[0])
+for key, value in file_data.items():
+    if 'csv' in value:
+        csv_file = value
+        print(csv_file)
+
+# with open(csv_file, 'r') as f:
+#     csv_reader = csv.reader(f)
+#     print(csv_reader)
